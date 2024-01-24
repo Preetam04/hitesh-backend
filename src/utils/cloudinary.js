dc2d2a1f91cae8c09763049a2381d35ba3f55c42
Preetam04 <preetampatil2324@gmail.com>
2024-01-24 18:02:31 +0530

    Delete prev avatar image &  cover image implemented

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,31 +1,48 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
-
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
-    try {
-        if (!localFilePath) return null
-        //upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
-        })
-        // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
-        fs.unlinkSync(localFilePath)
-        return response;
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfull
+    //console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
 
-    } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
-        return null;
+const removefromCloudinary = async (imageURL) => {
+  try {
+    const publicIdRegex = /\/([^\/]+)\.[a-zA-Z]{3,4}$/;
+    const match = imageURL.match(publicIdRegex);
+
+    if (match && match[1]) {
+      const publicId = match[1];
+      const response = await cloudinary.uploader.destroy(publicId, {
+        resource_type: "image",
+      });
+
+      console.log(response);
+      console.log("Image deleted Successfully from the server");
+    } else {
+      console.log("Public ID not found in the URL");
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-
-export {uploadOnCloudinary}
+export { uploadOnCloudinary, removefromCloudinary };
